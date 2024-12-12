bd0fe866e0b743bad5da9a7699ca5501c3b3f98d
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2024-12-12 13:22:12 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -4,6 +4,4 @@ echo "installing stpl RPI_CP1 Controller"
 sleep 1
 echo "Invented by SIJO MATHEW"
 sleep 1
-echo "please wait...."
-sleep 1
-sudo apt update && sudo apt upgrade && bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) && sudo systemctl enable nodered.service && cd ~/.node-red && npm i node-red-dashboard && node-red-restart && sudo reboot
+echo "please wait...." && sudo apt update && sudo apt upgrade && bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) && sudo systemctl enable nodered.service && cd ~/.node-red && npm i node-red-dashboard && node-red-restart && sudo reboot
