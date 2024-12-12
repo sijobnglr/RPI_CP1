6e8efd3d928f6203cfdc83451381cc17c10bfd60
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2024-12-12 12:30:24 +0530

    Delete rpi_cp1_installer.sh

--- rpi_cp1_installer.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "installing stpl RPI_CP1 Controller  Intended by SIJO MATHEW .. please wait"
-sleep 5
-sudo apt update && sudo apt upgrade && bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) && sudo systemctl enable nodered.service && cd ~/.node-red && npm i node-red-dashboard && node-red-restart && sudo reboot
